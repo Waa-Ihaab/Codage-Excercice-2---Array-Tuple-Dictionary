cc70e08088e084e3d94bb6c22dff1decc7ab4b21
Ihab <88992675+Waa-Ihaab@users.noreply.github.com>
2026-02-10 01:24:46 +0100

    Add examples of tuples, dictionaries, and arrays in Swift

--- main.swift
+++ main.swift
@@ -36,3 +36,138 @@ print("Last:", colors.last ?? "None")
 
 ///////////// 3. Création de Tuples /////////////
 
+import Foundation
+
+
+// Initializing tuples
+
+let coordonne2D = (x: 1, y: 5)
+
+
+let point3D = (x: 3, y: 6, z: 10)
+
+
+print("Coordonnée 2D : x = \(coordonne2D.x), y = \(coordonne2D.y)")
+
+print("Point 3D : x = \(point3D.x), y = \(point3D.y), z = \(point3D.z)")
+
+
+
+
+
+///////////// 4. Décomposition de Tuples /////////////
+
+let personne = (nom: "Pape", age: 27, poste: "Membre du clan")
+
+print("Nom :", personne.nom)
+print("Âge :", personne.age)
+print("Poste :", personne.poste)
+
+let (nom, age, poste) = personne
+
+print("Nom :", nom)
+print("Âge :", age)
+print("Poste :", poste)
+
+
+
+
+///////////// 5. Bases des Dictionnaires /////////////
+// Initializing an empty dictionary
+var ages = [String: Int]()
+
+ages["Pape"] = 27
+ages["Romano"] = 32
+
+ages["Pape"] = 28
+
+ages.removeValue(forKey: "Romano")
+
+for (name, age) in ages {
+    print("\(name) : \(age) ans")
+}
+
+
+
+
+
+
+
+
+
+///////////// 6. Dictionnaires Avancés /////////////
+var voiture: [String: String] = [
+    "couleur": "Noir",
+    "marque": "Mercedes",
+    "année": "1988"
+]
+
+print("Marque :", voiture["marque"] ?? "None")
+
+if let modele = voiture["modèle"] {
+    print("Modèle :", modele)
+} else {
+    print("Ops, modele non trouvee")
+}
+
+
+
+
+
+///////////// 7. Tri des Tableaux /////////////
+
+var numbers = [99, 5, 2, 21, 37, 5]
+
+let ascending = numbers.sorted()
+print("Ordre croissant :", ascending)
+
+let descending = numbers.sorted(by: >)
+print("Ordre décroissant :", descending)
+
+
+
+///////////// 8. Structures Mixtes /////////////
+var livres: [[String: String]] = []
+
+// 2. Ajouter trois livres au tableau
+livres.append([
+    "titre": "1984",
+    "auteur": "George Orwell",
+    "année": "1949"
+])
+
+livres.append([
+    "titre": "Le Petit Prince",
+    "auteur": "Antoine de Saint-Exupéry",
+    "année": "1943"
+])
+
+
+for livre in livres {
+    let titre = livre["titre"] ?? "Titre inconnu"
+    let auteur = livre["auteur"] ?? "Auteur inconnu"
+    
+    print("\(titre) - \(auteur)")
+}
+
+
+
+
+
+
+
+
+///////////// 9. Dictionnaires Complexes /////////////
+var motos: [String: [String]] = [
+    "Sportive": ["Yamaha R1", "Kawasaki Ninja ZX-10R", "Honda CBR1000RR"],
+    "Cruiser": ["Harley-Davidson Iron 883", "Indian Scout", "Honda Rebel 500"],
+    "Adventure": ["BMW GS 1250", "KTM Adventure 890", "Yamaha Ténéré 700"]
+]
+
+for (genre, motos) in motos {
+    print("\nGenre : \(genre)")
+    
+    for moto in motos {
+        print(" - \(moto)")
+    }
+}
